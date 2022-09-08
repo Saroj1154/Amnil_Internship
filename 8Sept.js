212a3af7ec9f16c57a5e89a053554fa0da34fb5d
saroj <pandeysarojofficial@gmail.com>
2022-09-08 22:57:15 +0545

    8th september tasks done: closure example

--- 8Sept.js
+++ 8Sept.js
@@ -1,12 +1,38 @@
-// closure example...
-const e = 10;
-function sum(a) {
+// Closures use lexical scoping and can inherit the variable from its parent function.
+// closure example with anonymous function...
+const x = 10;
+function add(a) {
     return function (b) {
         return function (c) {
             return function (d) {
-                return a + b + c + d + e;
+                return a + b + c + d + x;
             };
         };
     };
 };
-console.log(sum(1)(2)(3)(4)); // output:20
+console.log(add(1)(2)(3)(4)); // output:20
+
+
+
+//closure example with named function....
+// global scope
+const e = 10;
+function sum(a) {
+    return function sum2(b) {
+        return function sum3(c) {
+            // outer functions scope
+            return function sum4(d) {
+                // local scope
+                return a + b + c + d + e;
+            };
+        };
+    };
+}
+
+const sum2 = sum(1);
+const sum3 = sum2(2);
+const sum4 = sum3(3);
+const result = sum4(4);
+console.log(result); //output: 20
+
+
